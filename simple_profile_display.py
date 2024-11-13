a7d740ace37dde2cbb005f54067cf42b1e76033b
Sanaa Elmosbah <80988796+elmossanaa@users.noreply.github.com>
2024-11-13 14:48:20 -0500

    Create simple_profile_display.py

---
+++ simple_profile_display.py
@@ -0,0 +1,25 @@
+'''Create a short program that:
+   1. Stores personal information in variables.
+   2. Display a profile using print() statements.
+
+   Instructions:
+   1. Create a variable to store the following information about
+      a fictional character:
+      - name
+      - age
+      - favorite_color
+      - hobby
+
+   2. Use the print() function to display each piece of information
+      in a sentence format.'''
+
+ 
+name = "Blair Waldorf"
+age = 16
+favorite_color = "Navy, Red and Indigo"
+hobby = "Shopping"
+
+print("The character from the tv show Gossip girl is " + name)
+print(name + " is " + str(age) + " years old from season 1")
+print(name + "'s favorite colors are " + favorite_color)
+print(name + "'s hobby is " + hobby)
