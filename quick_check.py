f8e3200ab1ab02fd28a1f2eab0c08547b03144d2
Sanaa Elmosbah <80988796+elmossanaa@users.noreply.github.com>
2026-05-19 23:06:49 -0400

    Create quick_check.py

---
+++ quick_check.py
@@ -0,0 +1,37 @@
+import subprocess
+import datetime
+
+
+time = datetime.datetime.now().strftime("%m-%d-%y at %H:%M:%S")
+
+print("Hey There! \U0001F44B Let's run a quick system check!")
+
+while True:
+    print("\n Which would you like to run?")
+    print("1 - Ping Google")
+    print("2 - Show IP config")
+    print("3 - Exit")
+
+    choice = input ("Choose a number(1,2,3): ")
+
+    if choice == "1":
+        print("pinging google...\n")
+        c1 = subprocess.run(["ping","8.8.8.8"], capture_output = True, text = True)
+        print(f"Ping completed \U0001F600 on {time}  ")
+        print(c1.stdout)
+        print("Ping completed Successfully")
+        break
+
+    elif choice == "2":
+        print ("\n Showing IP config...\n")
+        c1 = subprocess.run(["ipconfig"], capture_output = True, text = True)
+        print(c1.stdout)
+        break
+
+    elif choice == "3":
+        print("Goodbye \U0001F641")
+        break
+
+    else:
+        print("Wrong input! Try again \U0001F61F")
+        
